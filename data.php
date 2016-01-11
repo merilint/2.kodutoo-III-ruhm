21042cb1077f83201afc437b227e497a594152db
Merilin Takk <meritak@greeny.cs.tlu.ee>
2016-01-11 02:30:49 +0200

    done

---
+++ data.php
@@ -0,0 +1,35 @@
+<?php
+	require_once("functions.php");
+	//data.php
+	// siia pääseb ligi sisseloginud kasutaja
+	//kui kasutaja ei ole sisseloginud,
+	//siis suuunan data.php lehele
+	if(!isset($_SESSION["logged_in_user_id"])){
+		header("Location: login.php");
+	}
+	
+	//kasutaja tahab välja logida
+	if(isset($_GET["logout"])){
+		//aadressireal on olemas muutuja logout
+		
+		//kustutame kõik session muutujad ja peatame sessiooni
+		session_destroy();
+		
+		header("Location: login.php");
+	}
+	
+	
+	
+	function cleanInput($data) {
+		$data = trim($data);
+		$data = stripslashes($data);
+		$data = htmlspecialchars($data);
+		return $data;
+	}
+	
+?>
+<p>
+	Tere, <?=$_SESSION["logged_in_user_email"];?> 
+	<a href="?logout=1"> Logi välja <a> 
+</p>
+
